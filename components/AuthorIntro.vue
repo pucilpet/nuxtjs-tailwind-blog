a09a8df5d98f7ffbc61b0bb0326a86ef46bfaf00
Petteri Pucilowski <petteri@hersecret.fi>
2023-08-28 12:38:23 +0300

    Update AuthorIntro.vue
    changed to text-center from justify

--- components/AuthorIntro.vue
+++ components/AuthorIntro.vue
@@ -5,27 +5,27 @@
     >
       👋 Hola, Hello, Moi!
     </h2>
-    <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
+    <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-center">
       Olen Petteri, kuopiolainen verkkokauppayrittäjä vuodesta 2010 lähtien. Olen tykännyt puuhastella tietokoneiden parissa pikkupojasta asti, opiskellut tietojenkäsittelytiedettä 
       Kuopion yliopistossa ja aktiivisesti täydentänyt omaa osaamista useilla eri verkkokursseilla. Olen työstänyt verkkosivuja lähes päivittäin yli vuosikymmenen ajan ja 
       pitkän linjan verkkokauppayrittäjänä tiedän, mitkä tekijät tekevät verkkosivuista hyvän ja edistävät myyntiä.
     </p>
-    <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
+    <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-center">
     (Melkein) kaikkien yritysten tavoitteena on myydä jotain - joko palveluita tai hyödykkeitä. Verkkosivujen avulla on tarkoitus tavoittaa ja saada uusia asiakkaita 
     sekä lisätä myyntiä. Nykypäivänä on entistä tärkeämpää, että yritys on esillä ja helposti tavoitettavissa netin kautta: yrityksellä on omat nettisivut, 
     somekanavat on kunnossa ja ajanvaraus tai tuotteiden ostaminen onnistuu myös netin välityksellä. Parantamalla yrityksen orgaanista näkyvyyttä netissä voi säästää mainoskuluissa ja saada uusia asiakkaita "ilmaiseksi".
     </p>
-    <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
+    <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-center">
     Mitä enemmän alalla on kilpailua, sitä tärkeämpää on erottua edukseen kilpailijoista. Moni kuluttaja vertailee eri vaihtoehtoja erityisesti ennen ensimmäistä käyntikertaa tai ostoa. 
     Verkkosivuttomat yritykset jäävät helposti heti ensimmäisenä potentiaalisten vaihtoehtojen ulkopuolelle. Jos kahdella potentiaalisella yrityksellä on molemmilla verkkosivut, 
     mutta vain toisella on käytössä nettiajanvaraus, valitsee suurin osa asiakkaista sen yrityksen, jolla nettiajanvaraus on käytössä. Jos molemmilla yrityksillä on 
     sekä verkkosivut että nettiajanvaraus käytettävissä, lähdetään kilpailuun uudesta asiakkaasta tasatilanteesta.
     </p>
-    <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
+    <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-center">
     Koska olen toiminut pienyrittäjänä itsekin jo yli vuosikymmenen ajan, pystyn samaistumaan muiden pienyrittäjien arkeen. Tarjoan palveluitani erityisesti muille pienyrittäjille 
     ja autan yritystäsi saavuttamaan täyden potentiaalinsa - kustannustehokkaaseen hintaan ja asiakaslähtöisesti.
     </p>
-    <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
+    <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-center">
     Verkkosivujen ja ajanvaraussysteemien lisäksi voimme auttaa somekanavien luomisessa, asiakaspalvelun kehittämisessä uudelle tasolle sekä automatisoimaan toistuvia työtehtäviä. 
     Erityisen paljon annettavaa minulla on muille verkkokauppayrittäjille tehokkaaseen asiakaspalveluun ja työtehtävien automatisointiin liittyen.
     </p>
